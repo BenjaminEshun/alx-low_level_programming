1b8c846dc23ee88dcbfc65998aaea5e630871753
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-09 10:58:01 -0700

    Added a file named 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets except q and e
+ *
+ * Return: program returns 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q' && alphabet == 'e')
+			continue;
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
