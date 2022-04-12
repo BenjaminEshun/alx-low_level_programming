be160aefc79526a8572d6b2f6f38d66408c524ec
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-12 15:10:05 -0700

    Added a file named 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /* Function prototype for 1_alphabet.c */
 void print_alphabet(void);
 
+/* Function prototype for 2_print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
 #endif
