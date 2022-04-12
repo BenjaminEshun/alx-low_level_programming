dd47333177f7aee31a393bfd8153dab0a89ac8c8
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-12 15:36:45 -0700

    Added afile named 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 
 /**
  * _islower - checkes for lowercase characters
- * @c - character to check
+ * @c: The characte or number to be check
  *
  * Return: On success 1 is returned.
  * On failure, 0 is returned
@@ -11,6 +11,6 @@
 int _islower(int c)
 {
 	if (islower(c) > 0)
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
