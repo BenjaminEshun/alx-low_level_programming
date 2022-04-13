e1bf32b28203259359df836df56c53e6a25c7c1d
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-12 17:41:00 -0700

    Adde a file named 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: The number to be computed
+ *
+ * Return: program returns the last digit og input value
+ */
+int print_last_digit(int x)
+{
+	int result;
+
+	if (x < 0)
+		x = -x;
+	result = x % 10;
+	_putchar(result + '0');
+	return(result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,4 +22,7 @@ int print_sign(int n);
 /* Function prototype for 6-abs.c */
 int _abs(int);
 
+/* Function prototype for 7-print_last_digit.c */
+int print_last_digit(int);
+
 #endif
