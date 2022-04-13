652d1ba3ff8744c0269946bcc8dd8f4b90591ef3
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-13 16:50:58 -0700

    Added a file named 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 98 Fibonacci numbers starting 1 and 2
+ *
+ * Return: program returns 0
+ */
+int main(void)
+{
+	unsigned long int i = 0, j = 1, result;
+	int count = 1;
+
+	while (count <= 98)
+	{
+		result = i + j;
+		i = j;
+		j = result;
+		printf("%lu", result);
+
+		if (count < 98)
+		{
+			printf(", ");
+		}
+
+		count += 1;
+	}
+	printf("\n");
+	return (0);
+}
