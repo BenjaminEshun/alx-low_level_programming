2c835fbc8f57500b803e6771d987df55cefb2fce
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-13 15:10:40 -0700

    Added a file named 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,23 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, result;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			printf("%3d", i * j);
+			result = i * j;
+			
+			if (result > 9)
+			{
+				_putchar(result/10 + '0');
+				_putchar(result%10 + '0');
+			}
+			else
+			{
+				_putchar(result%10 + '0');
+			}
 
 			if (j != 9)
 			{
