82c5cc157f76da4f4cf17dc6b75cf04ee22d0a7e
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-13 15:26:29 -0700

    Added a file named 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,14 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			result = i * j;	
+			result = i * j;
+
 			if (result > 9)
 			{
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
 			}
+
 			else
 			{
 				_putchar(result % 10 + '0');
