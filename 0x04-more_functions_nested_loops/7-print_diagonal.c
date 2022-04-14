e1a9b3b1d62e518aec2d516a69c1df84c14fd1ff
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-14 15:20:49 -0700

    Added a file named 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,24 @@
 
 /**
  * print_diagonal - draws a diagonal line
- * @n: number of diagonal lines
+ * @n: The number of times the character '\' should be printed
  *
- * Return: nothing is returned
+ * Return: program returns nothing
  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
