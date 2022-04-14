ae53b356db45bac89c07303c51d39105265daa92
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-14 16:19:35 -0700

    Added a file named 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100 with multiples of 3, 5 being Fizz, Buzz respectively
+ *
+ * Return: program returns 0
+ */
+int main(void)
+{
+	int num;
+	char multiples_of_3[] = "Fizz";
+	char multiples_of_5[] = "Buzz";
+	char multiples_of_3_and_5[] = "FizzBuzz";
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("%s ", multiples_of_3_and_5);
+		else if (num % 3 == 0)
+			printf("%s ", multiples_of_3);
+		else if (num % 5 == 0)
+			printf("%s ", multiples_of_5);
+		else
+			printf("%d ", num);
+	}
+	printf("\n");
+	return (0);
+}
