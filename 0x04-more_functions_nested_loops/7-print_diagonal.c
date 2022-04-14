aa64ce015974af2c63279d1fa40d45c6fa7c65e9
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-14 13:28:44 -0700

    Added a file named 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,16 @@
 void print_diagonal(int n)
 {
 	int i, j;
-
+	
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(32);
 		}
+
 		_putchar(92);
+
 		_putchar('\n');
 	}
 	_putchar('\n');
