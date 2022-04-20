dbb7e36b5234895f6200e7b7348c40b1ed76689a
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-20 13:55:14 -0700

    Added a file named 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string
+ *
+ * Return: nothing is returned
+ */
+void puts2(char *str)
+{
+	int string_length = strlen(str), i;
+
+	for (i = 0; i < string_length - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
