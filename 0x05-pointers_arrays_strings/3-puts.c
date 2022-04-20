53c57caa0f5a368ea689093d333e82642748df9a
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-20 12:49:19 -0700

    Added a file named 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string to be printed
+ *
+ * Return: nothing is returned
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
