051602ca89352ebfbd8208397d81069895012e4e
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-20 10:18:20 -0700

    Added a directory named 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as a parameter and update the value to 98
+ * @n: the pointer
+ *
+ * Return: nothing is returned
+ */
+void reset_to_98(int *n)
+{
+	int num = 98;
+
+	*n = num;
+}
