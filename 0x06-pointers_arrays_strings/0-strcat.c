39441398fe7c61ee15b528a4d8dacab64f94dbe2
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-22 07:10:59 -0700

    Added a directory named 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - function to concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: program returns a concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
