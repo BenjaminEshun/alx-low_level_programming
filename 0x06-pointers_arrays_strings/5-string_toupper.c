4a2508f6d40ae77aea424e7697a8e6c29e08d8f6
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-22 11:11:29 -0700

    Added a file named 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: the string
+ *
+ * Return: returns the string in uppercase
+ */
+char *string_toupper(char *str)
+{
+	return (strupr(str));
+}
