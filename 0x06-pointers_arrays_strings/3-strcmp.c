50e223f5f06714100c0bdaf13804b4b310b16844
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-22 07:51:29 -0700

    Added a file named 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns 0 if strings are equal, otherwise if not equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
