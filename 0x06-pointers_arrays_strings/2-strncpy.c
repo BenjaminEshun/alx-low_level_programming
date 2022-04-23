6131392f339e6be6e4c66ca9532f40304fe6b08c
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-23 09:15:06 -0700

    Added a file named 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,18 @@
+#include <string.h>
+#include "main.h"
 
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to be copied
+ *
+ * Return: program returns copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char result;
+
+	result = strncpy(dest, src, n);
+	return (result);
+}
