fafbf8adcb44b8e2a80e4bacd0ae01b1792e1672
Benjamin Eshun <eshunbenjamin69@gmail.com>
2022-04-28 15:12:24 -0700

    Added a new directory named 0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: the string
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
